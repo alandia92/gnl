d74adc3e6cb24e49671f24a2e9497f1af00ad236
Arrate Landia Iglesias <arlandia@c3r2s4.42urduliz.com>
2022-11-02 17:00:35 +0100

    arreglos

--- get_next_line.c
+++ get_next_line.c
@@ -3,46 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arrate <arrate@student.42.fr>              +#+  +:+       +#+        */
+/*   By: arlandia <arlandia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 12:37:12 by arrate            #+#    #+#             */
-/*   Updated: 2022/10/29 13:00:11 by arrate           ###   ########.fr       */
+/*   Updated: 2022/11/02 16:50:53 by arlandia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "get_next_line.h"
+#include "get_next_line.h"
 
-char    get_next_line(int fd)
+char	get_next_line(int fd)
 {
-    char        *line;
-    static char *
 }
 
-char    ft_getline(char *save)
+char	ft_getline(char *save)
 {
-    int     i;
-    char    *line;
-    
-    i = 0;
-    if (!save)
-        return (NULL);
-    i = ft_strlen(save);
-    line = malloc (sizeof(char) * (i + 2));
-    if (!line)
-        return (NULL);
-    i = 0;
-    while (save[i])
-    {
-        line[i] = save[i];
-        i++;
-    }
-    if (save[i] = '\n')
-    {
-        line[i] = '\n';
-        line[i + 1] = '\0';
-    }
-    else if(save[i] = '\0')
-        line[i] = '\0';
-    return(line);
-}
+	int		i;
+	char	*line;
 
+	i = 0;
+	if (!save)
+		return (NULL);
+	i = ft_strlen(save);
+	line = malloc (sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (save[i])
+	{
+		line[i] = save[i];
+		i++;
+	}
+	if (save[i] = '\n')
+	{
+		line[i] = '\n';
+		line[i + 1] = '\0';
+	}
+	else if (save[i] = '\0')
+		line[i] = '\0';
+	return (line);
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,84 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arrate <arrate@student.42.fr>              +#+  +:+       +#+        */
+/*   By: arlandia <arlandia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 12:39:30 by arrate            #+#    #+#             */
-/*   Updated: 2022/10/29 12:56:07 by arrate           ###   ########.fr       */
+/*   Updated: 2022/11/02 16:44:46 by arlandia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "get_next_line.h"
+#include "get_next_line.h"
 
-char    *ft_strchr(char *s, int c)
+char	*ft_strchr(char *s, int c)
 {
-    int i;
-    
-    i = 0;
-    if (!s)
-        return(NULL);
-    while (s[i] == (char) c)
-        return ((char *) &s[i]);
-    i++;
-    return(0);
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] == (char) c)
+		return ((char *) &s[i]);
+	i++;
+	return (0);
 }
 
-size_t  ft_strlen(const char *s)
+size_t 	ft_strlen(const char *s)
 {
-    size_t i;
-    
-    i = 0;
-    while (s [i])
-        i++;
-    return (i);
+	size_t	i;
+
+	i = 0;
+	while (s [i])
+		i++;
+	return (i);
 }
 
-char    *ft_strdup(char const *s1)
+char	*ft_strdup(char const *s1)
 {
-    char    *str;
-    size_t  i;
-    
-    if(!s1)
-        return(NULL);
-    while (s1[i])
-        i++;
-    str = malloc(sizeof (char) * s1[i+1]);
-    if(!str)
-    {
-        free (s1);
-        return (NULL);
-    }
-    i = 0;
-    while (s1[i])
-    {
-        str[i] = s1[i];
-        i++;
-    }
-    str[i] = 0;
-    free(s1);
-    return(str[i]);
+	char	*str;
+	size_t	i;
+
+	if (!s1)
+		return (NULL);
+	while (s1[i])
+		i++;
+	str = malloc(sizeof (char) * s1[i+1]);
+	if (!str)
+	{
+		free (s1);
+		return (NULL);
+	}
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = 0;
+	free(s1);
+	return (str[i]);
 }
 
-char *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    size_t  i;
-    size_t  j;
-    int     len;
-    char    *str;
+	size_t	i;
+	size_t	j;
+	int		len;
+	char	*str;
 
-    if(!s1 && !s2)
-        return (NULL);
-    str = malloc (sizeof (char) * (len + 1));
-    if (!str)
-        return(NULL);
-    i = -1;
-    while(s1[++i])
-        str[i] = s1[i];
-    j = -1;
-    while (s2 [j])
-        str[i++] = s2 [j++];
-    str[len] = 0;
-    free(s1);
-    return (str);
+	if (!s1 && !s2)
+		return (NULL);
+	str = malloc (sizeof (char) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		str[i] = s1[i];
+	j = -1;
+	while (s2 [j])
+		str[i++] = s2 [j++];
+	str[len] = 0;
+	free(s1);
+	return (str);
 }
-
